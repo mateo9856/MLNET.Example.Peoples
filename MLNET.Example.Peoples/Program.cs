76b805b21a1d9a560a376c5664995a97b8a96ad9
mateo9856 <mateo9656@gmail.com>
2023-05-05 17:59:21 +0200

    Add project files.

---
+++ MLNET.Example.Peoples/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.ML;
+using MLNET.Example.Peoples;
+using MLNET_Example_Peoples;
+
+Console.WriteLine("ML NET Learning");
+
+MLContext mlContext = new MLContext();
+
+string dataPath = Path.Combine(Directory.GetCurrentDirectory(), "people-100.csv");
+
+IDataView dataView = mlContext.Data.LoadFromTextFile<PeopleModel>(dataPath);
+
+//Predict data
