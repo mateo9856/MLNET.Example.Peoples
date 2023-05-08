e874f35fb155985dcbe9b2fb59a72dc0010829c8
mateo9856 <mateo9656@gmail.com>
2023-05-08 22:57:45 +0200

    multiclass classification

--- MLNET.Example.Peoples/Program.cs
+++ MLNET.Example.Peoples/Program.cs
@@ -10,4 +10,16 @@ string dataPath = Path.Combine(Directory.GetCurrentDirectory(), "people-100.csv"
 
 IDataView dataView = mlContext.Data.LoadFromTextFile<PeopleModel>(dataPath);
 
-//Predict data
+IEstimator<ITransformer> dataPipeline = mlContext.Transforms.Conversion.MapValueToKey("Sex", "Label")
+    .Append(mlContext.Transforms.Text.FeaturizeText(nameof(PeopleModel.FirstName), "FirstNameFeaturized"))
+    .Append(mlContext.Transforms.Text.FeaturizeText(nameof(PeopleModel.LastName), "LastNameFeaturized"))
+    .Append(mlContext.Transforms.Text.FeaturizeText(nameof(PeopleModel.Email), "EmailFeaturized"))
+    .Append(mlContext.Transforms.Concatenate("Features", "FirstNameFeaturized", "LastNameFeaturized", "EmailFeaturized"))
+    .AppendCacheCheckpoint(mlContext);
+
+var trainingPipeline = dataPipeline.Append(mlContext.MulticlassClassification.Trainers.SdcaMaximumEntropy("Label", "Features"))
+    .Append(mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
+
+ITransformer model = trainingPipeline.Fit(dataView);
+
+var engine = mlContext.Model.CreatePredictionEngine<PeopleModel, PeopleModelOutput>(model);
