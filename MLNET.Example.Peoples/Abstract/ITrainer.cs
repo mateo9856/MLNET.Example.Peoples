440aa7bc5d71f8b155bed5491c1653731fc00196
mateo9856 <mateo9656@gmail.com>
2023-05-15 22:22:46 +0200

    prepare ITrainer

---
+++ MLNET.Example.Peoples/Abstract/ITrainer.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.ML;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MLNET.Example.Peoples.Abstract
+{
+    public interface ITrainer<T> where T : class
+    {
+        void Fit(string filePath);
+        void SaveToFile();
+
+    }
+}
